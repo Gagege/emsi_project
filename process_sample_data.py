c31429b85aeb96e53a6ec072952b13177834cdeb
Gage Herrmann <gage.herrmann@carsforsale.com>
2019-11-18 10:43:20 -0600

    Add body and title to database

--- process_sample_data.py
+++ process_sample_data.py
@@ -1,17 +1,48 @@
 import gzip
 import json
 import re
+import sqlite3
 
-html_pattern = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+html_pattern = re.compile('<.*?>')
+connection = sqlite3.connect('interview_project.db')
+
+def create_new_listings_table():
+    c = connection.cursor()
+    c.execute('''DROP TABLE IF EXISTS listings''')
+    c.execute('''CREATE TABLE listings
+                (body text, title text, expired text, posted text, state text, city text, onet text, soc5 text, soc2 text)''')
+    connection.commit()
 
 def remove_html_tags(text):
     clean_text = re.sub(html_pattern, '', text)
     return clean_text
 
-with gzip.open('data/sample.gz', 'r') as fileIn:
-    num = 0
-    for line in fileIn:
-        listing = json.loads(line)
-        body = listing['body'].encode('utf8')
-        clean_body = remove_html_tags(body)
-        print(clean_body)
+def insert_listing(body, title):
+    c = connection.cursor()
+    c.execute('INSERT INTO listings VALUES (?,?,?,?,?,?,?,?,?)', [
+        buffer(body),
+        buffer(title),
+        'expired',
+        'posted',
+        'state',
+        'city', 
+        'onet', 'soc5', 'soc2'])
+    connection.commit()
+
+def process_line(line):
+    listing_json = json.loads(line)
+    body = listing_json['body'].encode('utf8')
+    clean_body = remove_html_tags(body)
+    insert_listing(
+        body = clean_body,
+        title = listing_json['title']
+        )
+
+def process_file(file_location):
+    with gzip.open(file_location, 'r') as fileIn:
+        for line in fileIn:
+            process_line(line)
+
+create_new_listings_table()
+process_file('data/sample.gz')
+connection.close()
