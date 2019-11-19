447cace3acf806b1bfbf0bec38a68d60ed9658c1
Gage Herrmann <gage.herrmann@carsforsale.com>
2019-11-18 22:48:44 -0600

    Add posted and expired dates to database

--- process_sample_data.py
+++ process_sample_data.py
@@ -2,6 +2,7 @@ import gzip
 import json
 import re
 import sqlite3
+from datetime import datetime
 
 html_pattern = re.compile('<.*?>')
 connection = sqlite3.connect('interview_project.db')
@@ -10,36 +11,47 @@ def create_new_listings_table():
     c = connection.cursor()
     c.execute('''DROP TABLE IF EXISTS listings''')
     c.execute('''CREATE TABLE listings
-                (body text, title text, expired text, posted text, state text, city text, onet text, soc5 text, soc2 text)''')
+                (body text, title text, expired date, posted date, state text, city text, onet text, soc5 text, soc2 text)''')
     connection.commit()
 
 def remove_html_tags(text):
     clean_text = re.sub(html_pattern, '', text)
     return clean_text
 
-def insert_listing(body, title):
+def to_datetime(date_str):
+    return datetime.strptime(date_str, '%Y-%m-%d')
+
+def insert_listing(body, title, expired, posted, state, city, onet, soc5, soc2):
     c = connection.cursor()
     c.execute('INSERT INTO listings VALUES (?,?,?,?,?,?,?,?,?)', [
-        buffer(body),
-        buffer(title),
-        'expired',
-        'posted',
-        'state',
-        'city', 
-        'onet', 'soc5', 'soc2'])
+        buffer(body), title, 
+        expired.isoformat(), 
+        posted.isoformat(), 
+        state, city, 
+        onet, soc5, soc2])
     connection.commit()
 
 def process_line(line):
     listing_json = json.loads(line)
     body = listing_json['body'].encode('utf8')
     clean_body = remove_html_tags(body)
+    expired = to_datetime(listing_json['expired'])
+    posted = to_datetime(listing_json['posted'])
     insert_listing(
         body = clean_body,
-        title = listing_json['title']
+        title = listing_json['title'],
+        expired = expired,
+        posted = posted,
+        state = listing_json['state'],
+        city = listing_json['city'],
+        onet = listing_json['onet'],
+        soc5 = 'soc5',
+        soc2 = 'soc2',
         )
 
 def process_file(file_location):
     with gzip.open(file_location, 'r') as fileIn:
+        # process_line(fileIn.readline())
         for line in fileIn:
             process_line(line)
 
