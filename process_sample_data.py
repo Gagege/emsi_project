3fd538d22483e7d05a67e8ae22b79d0766d110ec
Gage Herrmann <gage.herrmann@carsforsale.com>
2019-11-20 07:14:32 -0600

    Finished insertions and added comments

--- process_sample_data.py
+++ process_sample_data.py
@@ -5,7 +5,11 @@ import csv
 import sqlite3
 from datetime import datetime
 
+# There are some broken html tags and javascript in the sample.
+# I did some research and decided it wouldn't be the best use of my time to try to remove every bit of broken html and code.
+# So I'm simply removing all the actual html tags.
 html_pattern = re.compile('<.*?>')
+html_removed_count = 0
 connection = sqlite3.connect('interview_project.db')
 
 def create_new_listings_table():
@@ -20,6 +24,7 @@ def remove_html_tags(text):
     return clean_text
 
 def to_datetime(date_str):
+    # I would usually put a try/except here but the dates in the sample data are all nice a clean
     return datetime.strptime(date_str, '%Y-%m-%d')
 
 def insert_listing(body, title, expired, posted, state, city, onet, soc5, soc2):
@@ -33,42 +38,62 @@ def insert_listing(body, title, expired, posted, state, city, onet, soc5, soc2):
     connection.commit()
 
 def process_line(line, map_onet_soc, soc_hierarchy):
-    listing_json = json.loads(line)
-    body = listing_json['body'].encode('utf8')
-    clean_body = remove_html_tags(body)
-    expired = to_datetime(listing_json['expired'])
-    posted = to_datetime(listing_json['posted'])
-    insert_listing(
-        body = clean_body,
-        title = listing_json['title'],
-        expired = expired,
-        posted = posted,
-        state = listing_json['state'],
-        city = listing_json['city'],
-        onet = listing_json['onet'],
-        soc5 = 'soc5',
-        soc2 = 'soc2',
-        )
+    try:
+        listing_json = json.loads(line)
+        body = listing_json['body'].encode('utf8')
+        clean_body = remove_html_tags(body)
+        if body != clean_body:
+            global html_removed_count
+            html_removed_count += 1
+        expired = to_datetime(listing_json['expired'])
+        posted = to_datetime(listing_json['posted'])
+        soc5 = map_onet_soc[listing_json['onet']]
+        
+        # some soc5 codes are not in the soc_hierarchy file
+        try:
+            soc2 = soc_hierarchy[soc5]
+        except KeyError:
+            soc2 = None
+
+        insert_listing(
+            body = clean_body,
+            title = listing_json['title'],
+            expired = expired,
+            posted = posted,
+            state = listing_json['state'],
+            city = listing_json['city'],
+            onet = listing_json['onet'],
+            soc5 = soc5,
+            soc2 = soc2,
+            )
+    except KeyError:
+        print('Unable to parse this listing:')
+        print(listing_json)
+
+def map_onet_soc_csv_to_dict(file_location):
+    with open(file_location, 'r') as f:
+        mapping = {}
+        for row in csv.DictReader(f):
+            mapping[row['onet']] = row['soc5']
+        return mapping
+
+def soc_hierarchy_to_dict(file_location):
+    with open(file_location, 'r') as f:
+        mapping = {}
+        for row in csv.DictReader(f):
+            mapping[row['child']] = row['parent']
+        return mapping
 
 def process_file(file_location):
-    sample_f = gzip.open(file_location, 'r')
-    map_onet_soc_f = open('data/map_onet_soc.csv', 'r')
-    soc_hierarchy_f = open('data/soc_hierarchy.csv', 'r')
-
-    try:
-        map_onet_soc = csv.DictReader(map_onet_soc_f)
-        soc_hierarchy = csv.DictReader(soc_hierarchy_f)
-
-        process_line(sample_f.readline(), map_onet_soc, soc_hierarchy)
-        # for line in sample_f:
-        #     process_line(line)
-    finally:
-        sample_f.close()
-        map_onet_soc_f.close()
-        soc_hierarchy_f.close()
+    with gzip.open(file_location, 'r') as sample_f:
+        map_onet_soc = map_onet_soc_csv_to_dict('data/map_onet_soc.csv')
+        soc_hierarchy = soc_hierarchy_to_dict('data/soc_hierarchy.csv')
+        for line in sample_f:
+            process_line(line, map_onet_soc, soc_hierarchy)
         
 try:
     create_new_listings_table()
     process_file('data/sample.gz')
+    print('Removed html tags from this many listings:', html_removed_count)
 finally:
     connection.close()
